0f52bce14ec76dfedc74ed3b86100ff21d52d186
Pranjal-mahajan113 <pranjalmahajan2329@gmail.com>
2026-03-04 19:30:06 +0530

    Reverse string question slove

---
+++ 4 MARCH/ReverseString.js
@@ -0,0 +1,8 @@
+function reverseWords(sentence) {
+    let reverse = [];
+    for(let i =sentence.length-1; i<sentence.length;i--){
+        console.log(i)
+    }
+
+}
+console.log(reverseWords("Hello World"))
