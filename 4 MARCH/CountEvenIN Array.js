b5c47be0ec827aa783f857a924481743f43cc5cf
Pranjal-mahajan113 <pranjalmahajan2329@gmail.com>
2026-03-05 21:23:48 +0530

    Add edge condition

--- 4 MARCH/CountEvenIN Array.js
+++ 4 MARCH/CountEvenIN Array.js
@@ -1,13 +1,23 @@
 function countEvens(arr) {
-  if(!Array.isArray(arr)) return false;
+  if (!Array.isArray(arr)) return false;
+  if (arr.length === 0) {
+    return 0;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] !== "number" || !Number.isFinite(arr[i])) {
+      return false;
+    }
+  }
+
   let count = 0;
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] % 2 === 0) {
-     count++;
+      count++;
     }
   }
   return count;
 }
 
 console.log(countEvens([1, 2, 3, 4]));
-console.log(countEvens([-2,-5,-8]))
+console.log(countEvens([-2, -5, -8]));
+console.log(countEvens([]));
