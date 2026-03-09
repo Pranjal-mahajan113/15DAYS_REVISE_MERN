3ed69d04c3692f3ec7dbc617ed7f95668758a93c
Pranjal-mahajan113 <pranjalmahajan2329@gmail.com>
2026-03-09 20:32:15 +0530

    Find missing number using reduce

--- 8_9_march/Findmissing_num.js
+++ 8_9_march/Findmissing_num.js
@@ -8,3 +8,21 @@ function findmissing(arr) {
   }
 }
 console.log(findmissing([3, 0, 1]));
+
+//2.using Reduce + sum of number formula==========================
+function findMissing2(nums2) {
+  // array length
+  let n = nums2.length;
+
+  // expected sum (0 se n)
+  let expected = (n * (n + 1)) / 2;
+
+  // reduce se array ka sum
+  let actual = nums2.reduce((sum, num) => sum + num, 0);
+
+  // difference hi missing number hai
+  return expected - actual;
+}
+
+
+console.log("Reduce Method:", findMissing2([0,3,1]));
