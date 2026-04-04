4b7a1927ca269fefcfb84030177c961f6d1428c0
Pranjal-mahajan113 <pranjalmahajan2329@gmail.com>
2026-04-04 10:37:01 +0530

    filter Transfrom

---
+++ APRIL/Filterelement.js
@@ -0,0 +1,11 @@
+var filter = function (arr, fn) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+   if(fn(arr[i],i)){
+    result.push(arr[i])
+   }
+  }
+  return result;
+};
+
+console.log(filter([0, 10, 20, 30], (n) => n > 10));
