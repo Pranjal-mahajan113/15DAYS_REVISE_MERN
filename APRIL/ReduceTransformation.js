6589a80b2a66fab5253e50c9f8eff0acd7abe516
Pranjal-mahajan113 <pranjalmahajan2329@gmail.com>
2026-04-04 11:12:51 +0530

    function composition

---
+++ APRIL/ReduceTransformation.js
@@ -0,0 +1,8 @@
+var reduce = function (nums, fn, init) {
+  let val = init;
+  for (let i = 0; i < nums.length; i++) {
+    val = fn(val, nums[i]);
+  }
+  return val;
+};
+console.log(reduce([1, 2, 3, 4], (acc, curr) => acc + curr,0));
