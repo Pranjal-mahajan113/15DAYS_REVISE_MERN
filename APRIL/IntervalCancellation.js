00abc3cadee1deef18fa976734568e14e5f5ecbe
Pranjal-mahajan113 <pranjalmahajan2329@gmail.com>
2026-04-17 18:01:57 +0530

    Console error

--- APRIL/IntervalCancellation.js
+++ APRIL/IntervalCancellation.js
@@ -1,11 +1,13 @@
-var cancellable = function(fn, args, t) {
-    fn(...args);
+var cancellable = function (fn, args, t) {
+  fn(...args);
 
-    let intervalId = setInterval(() => {
-        fn(...args);
-    }, t);
+  let intervalId = setInterval(() => {
+    console.log(fn(...args));
+  }, t);
 
-    return function cancelFn() {
-        clearInterval(intervalId);
-    };
+  return function cancelFn() {
+    clearInterval(intervalId);
+  };
 };
+const cancelFn = cancellable((x) => x * 2, [4], 35);
+setTimeout(cancelFn, 100);
