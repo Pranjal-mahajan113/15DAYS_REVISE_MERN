c0301ae2bb85ca9f0e462011234a0401f9fc82ab
Pranjal-mahajan113 <pranjalmahajan2329@gmail.com>
2026-08-18 11:35:56 +0530

    Frequency count

--- AUGUST/18August.js
+++ AUGUST/18August.js
@@ -1,43 +1,73 @@
-function sumOfArray(arr) {
-  let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    sum = sum + arr[i];
-  }
-  return sum;
-}
-console.log(sumOfArray([1, 2, 3, 4, 5]));
+// function sumOfArray(arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sum = sum + arr[i];
+//   }
+//   return sum;
+// }
+// console.log(sumOfArray([1, 2, 3, 4, 5]));
 
-function countEvenNumber(arr2) {
-  let count = 0;
-  let sum = 0;
-  for (let i = 0; i < arr2.length; i++) {
-    if (arr2[i] % 2 === 0) {
-      count++;
-      sum = sum + arr2[i];
-    }
-  }
-  return [count, sum];
-}
-console.log(countEvenNumber([1, 2, 3, 4, 5, 6]));
+// function countEvenNumber(arr2) {
+//   let count = 0;
+//   let sum = 0;
+//   for (let i = 0; i < arr2.length; i++) {
+//     if (arr2[i] % 2 === 0) {
+//       count++;
+//       sum = sum + arr2[i];
+//     }
+//   }
+//   return [count, sum];
+// }
+// console.log(countEvenNumber([1, 2, 3, 4, 5, 6]));
 
-function MaxNumberArray(arr3) {
-  let max = arr3[0];
-  for (let i = 1; i < arr3.length; i++) {
-    if (arr3[i] > max) {
-      max = arr3[i];
-    }
-  }
-  return max;
-}
-console.log(MaxNumberArray([12, 5, 89, 34, 67]));
+// function MaxNumberArray(arr3) {
+//   let max = arr3[0];
+//   for (let i = 1; i < arr3.length; i++) {
+//     if (arr3[i] > max) {
+//       max = arr3[i];
+//     }
+//   }
+//   return max;
+// }
+// console.log(MaxNumberArray([12, 5, 89, 34, 67]));
 
-function MinNumberArray(arr4) {
-  let min = arr4[0];
-  for (let i = 1; i < arr4.length; i++) {
-    if (arr4[i] < min) {
-      min = arr4[i];
-    }
-  }
-  return min;
+// function MinNumberArray(arr4) {
+//   let min = arr4[0];
+//   for (let i = 1; i < arr4.length; i++) {
+//     if (arr4[i] < min) {
+//       min = arr4[i];
+//     }
+//   }
+//   return min;
+// }
+// console.log(MinNumberArray([12, 5, 89, 34, 67]));
+
+// function searchElement(arr, target) {
+//     for(let i=0;i<arr.length;i++){
+//         if(arr[i]===target){
+//             return" Found";
+//         }
+//     }
+//     return "Not found"
+
+// }
+// console.log(searchElement([10, 20, 30, 40, 50],99))
+
+// function countFrequency(arr, target) {
+//   let count = 0;
+
+//   // code here
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === target) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+
+// console.log(countFrequency([1, 2, 2, 3, 2, 4], 2));
+
+function countFrequencyWithFilter(arr, target) {
+  return arr.filter((item) => item === target).length;
 }
-console.log(MinNumberArray([12, 5, 89, 34, 67]));
+console.log(countFrequencyWithFilter([1, 2, 2, 3, 2, 4], 2));
