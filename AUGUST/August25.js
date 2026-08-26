60137a88b862e79c6b35b3052c662eceb2ab8ccd
Pranjal-mahajan113 <pranjalmahajan2329@gmail.com>
2026-08-26 14:49:22 +0530

    TWO sum

---
+++ AUGUST/August25.js
@@ -0,0 +1,19 @@
+// function findDuplicate(arr) {
+//   let seen = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (seen.includes(arr[i])) {
+//       return arr[i];
+//     } else {
+//       seen.push(arr[i]);
+//     }
+//   }
+// }
+// console.log(findDuplicate([1, 3, 4, 2, 2, 6]));
+
+function TwoSum(arr2,target){
+    
+for(let i=0;i<arr2.length;i++){
+    let needed = target-arr2[i];
+}
+}
+console.log(TwoSum([2, 7, 11, 15],9))
